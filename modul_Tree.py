3f360c4c4334dadcd972d958f9c301f7cf4e31ee
Lakioneo <Lakioneo@gmail.com>
2022-08-22 21:28:30 +0300

    Добовление веток в дерево+

--- modul_Tree.py
+++ modul_Tree.py
@@ -3,7 +3,23 @@ class Tree ():
         main = None
         left = None
         right = None
+    
     def add_point(self, num):
-        pass
+        if self.main == None:
+            self.main = num
+            return
+        
+        if self.main < num:
+            if self.left == None:
+                self.left = Tree().main = num
+            else:
+                self.left.add_point(num)
+                
+        else:
+            if self.right == None:
+                self.right = Tree().main = num
+            else:
+                self.right.add_point(num)
+
     def print_tree(self):
         pass
